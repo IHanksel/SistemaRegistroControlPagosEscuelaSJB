e2fd7170bd1b7f8a20f2a37ba4c145dde642caf9
jandro29 <ascolanr@gmail.com>
2025-05-07 01:49:30 -0500

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/proyecto-SJB',
+  basePath: '/SistemaRegistroControlPagosEscuelaSJB',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,24 +2,24 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/proyecto-SJB/',
+  baseHref: '/SistemaRegistroControlPagosEscuelaSJB/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/proyecto-SJB"
+    "route": "/SistemaRegistroControlPagosEscuelaSJB"
   },
   {
     "renderMode": 2,
-    "route": "/proyecto-SJB/inicio-control-de-pagos"
+    "route": "/SistemaRegistroControlPagosEscuelaSJB/inicio-control-de-pagos"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 28564, hash: 'eac97c19fc9e3b9ce5359e630b9f5a5ccbf4677548cb9ac7e41e55b2bfcaea2d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17152, hash: 'c23928128a2ba08c4d5922b5f9e3452d1ae9924ac915672cbc1a9887602a1b2b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'inicio-control-de-pagos/index.html': {size: 33801, hash: '71b22c086f44b95e72f9afb59f092cdb767a0db06f18c69b19a77172f6fcd696', text: () => import('./assets-chunks/inicio-control-de-pagos_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 87755, hash: 'f8dcf096411d272142aa22f04b91b6bfb88a098fb9d09380605a0402ee1e4683', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 28589, hash: 'db3a8454b785be605b34208fa55e0307c9063c5f42c74bbc530eaee880eeb5de', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17177, hash: 'c25cc6ec74c988b83ec6189ca688f050e55d1e593861938f93a958ca4672c797', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'inicio-control-de-pagos/index.html': {size: 33826, hash: '3aebf798e825d3f21d6fd656289daca3860d1f99d379acc86871e0248e37d55d', text: () => import('./assets-chunks/inicio-control-de-pagos_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 87780, hash: 'ba8e4e19022cd3d241c79df125b3f6c1a2e21ed5982c5bb08b0dc030705fc205', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5YCQL7LF.css': {size: 20263, hash: 'Ol7ROH0dcPE', text: () => import('./assets-chunks/styles-5YCQL7LF_css.mjs').then(m => m.default)}
   },
 };
